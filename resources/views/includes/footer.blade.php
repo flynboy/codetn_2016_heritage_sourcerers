dbcc7207cc11df8548d1869434bd030c0efaba19
Joe Simpson <flynboy@gmail.com>
2016-09-28 19:50:25 +0000

    test upate

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container">
     <ul>
-      <li class="text-muted"><a href="http://codetn.org">CodeTN.org</a></li>
+      <li class="text-muted"><a href="http://codetn.org">CodeTN.org (bam)</a></li>
       <li class="text-muted tall"><a href="maps:ll=35.961254,-83.915499">912 S Gay St,<br />Knoxville, TN 37902</a></li>
       <li class="text-muted"><a href="tel:18652154501">(865) 215-4501</a></li>
       <li class="text-muted"><a href="mailto:info@codetn.org">info@codetn.org</a></li>
